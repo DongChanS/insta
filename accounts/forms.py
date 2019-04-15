fb4bc2c108df2240504dd384ce4964eab3cc5c81
Dong Chan <start6663@gmail.com>
2019-04-15 05:06:54 +0000

    people 페이지 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,10 @@
 from django import forms
-from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class UserModelForm(forms.ModelForm):
     
     class Meta:
-        # model = settings.AUTH_USER_MODEL
-        model = User
+        model = get_user_model()
         fields = ['username','password']
         
         widgets = {
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from .forms import UserModelForm
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
 """
 1. 로그인 폼을 만든다.
     -> 이건 user데이터베이스를 상속해서 만들어야할듯?
@@ -49,4 +50,11 @@ def signup(request):
         return render(request, 'accounts/signup.html', {
             'form' : form
         })
-    
+
+def people(request, username):
+    person = get_object_or_404(get_user_model(), username=username)
+    # 1. settings.AUTH_USER_MODEL (django.conf) -> 얘는 view에서 못쓴다고함.
+    # 2. get_user_model() (django.contrib.auth import get_user_model())
+    return render(request, 'accounts/people.html', {
+        'person':person
+    })
