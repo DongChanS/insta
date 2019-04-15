5359766898b46be87a50c9dcaf2c361d1b8653b3
Dong Chan <start6663@gmail.com>
2019-04-15 04:41:45 +0000

    회원가입 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from .forms import UserModelForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 """
 1. 로그인 폼을 만든다.
     -> 이건 user데이터베이스를 상속해서 만들어야할듯?
@@ -32,3 +32,21 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('accounts:login')
+    
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            auth_login(request, form.instance)
+            # 여기서는 get_user 못씀!
+            return redirect('posts:list')
+        else:
+            return redirect('accounts:signup')
+        
+    else:
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {
+            'form' : form
+        })
+    
